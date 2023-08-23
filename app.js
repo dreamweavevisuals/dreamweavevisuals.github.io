c86e172cf3e78c9b432c66994d7bf867f142f365
dreamweavevisuals <140268844+dreamweavevisuals@users.noreply.github.com>
2023-08-23 18:39:07 -0400

    Website V0.5
    Added Hidden Navigation on Scroll

--- app.js
+++ app.js
@@ -42,6 +42,30 @@ const navSlide = () => {
 
 navSlide();
 
+//Hide Navigation on Scroll
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+	const currentScroll = window.pageYOffset;
+	if (currentScroll <= 0) {
+		body.classList.remove("scroll-up");
+		return;
+	}
+
+	if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+		body.classList.remove("scroll-up");
+		body.classList.add("scroll-down");
+	} else if (
+		currentScroll < lastScroll &&
+		body.classList.contains("scroll-down")
+	) {
+		body.classList.remove("scroll-down");
+		body.classList.add("scroll-up");
+	}
+	lastScroll = currentScroll;
+});
+
 const swiper = new Swiper('#swiper-1', {
     slidesPerView: 1,
     centeredSlides: true,

edaa8b9b55730d686fc603ce0d52f0a21f903792
dreamweavevisuals <140268844+dreamweavevisuals@users.noreply.github.com>
2023-09-27 19:18:49 -0400

    Website V0.9
    - Added Fancybox on Photography Page
    
    - Created Lightbox for Landscape Category
    
    - Mobile Formatting

--- app.js
+++ app.js
@@ -66,6 +66,18 @@ window.addEventListener("scroll", () => {
 	lastScroll = currentScroll;
 });
 
+Fancybox.bind("[data-fancybox]", {
+  // Your custom options
+  contentClick: 'close',
+  Toolbar: {
+    display: {
+      left: [],
+      middle: ["infobar"],
+      right: ["thumbs", "close"],
+    },
+  },
+});
+
 const swiper = new Swiper('#swiper-1', {
     slidesPerView: 1,
     centeredSlides: true,
